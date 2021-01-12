0522c7fe6b28421ddce9577d26001628a81bbaa2
Rafin Akhmad F <rafinakhmad.siswa@smkn4bdg.sch.id>
2021-01-12 22:30:05 +0700

    search

---
+++ app/src/main/java/com/example/nusantarafit/search.java
@@ -0,0 +1,15 @@
+package com.example.nusantarafit;
+
+        import android.os.Bundle;
+        import androidx.appcompat.app.AppCompatActivity;
+
+public class search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout2);
+
+        getSupportActionBar().setTitle("Search");
+    }
+}
